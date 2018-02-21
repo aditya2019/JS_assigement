14ba5c093b6d46ceeba076e584b7467fa5e0e56b
adityapalmehula <javajiadityapal@gmail.com>
2018-02-21 14:06:07 +0530

    done

--- task-6/isnumber.js
+++ task-6/isnumber.js
@@ -1,28 +1,7 @@
-const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('Please enter your number which you want to find stirng or number : ', (answer1) => {
-
-
-      var number=answer1;
-
-
-  if(typeof(number)===typeof(adi))
-  {
-      console.log("string");
-  }
-  if(typeof(number)===typeof(2334))
-  {
-      console.log("number");
-  }
-
-
-
-
-
-
-});
+ var num1 = 121;
+ if(isNaN(num1)){
+	console.log(num1 + " is not a number <br/>");
+ }else{
+	console.log(num1 + " is a number <br/>");
+ }
